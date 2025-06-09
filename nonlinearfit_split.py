Added standard deviation for the parameters and fixed axis scaling

nonlinearfit_split.py
@@ -67,6 +67,11 @@ def ModelFit(filepath,i):
             )
             df["model"] = f_model_diff_m(df["x"], popt[0], popt[1])
     
+    rows =  df.shape[0]
+    q = popt[0] - rows/2
+    perr = np.sqrt(np.diag(pcov))
+    Dq, Dd = perr
+
     # Create the plot
     fig, ax = plt.subplots()
     axb = ax.twinx()
@@ -85,7 +90,7 @@ def ModelFit(filepath,i):
     sparser_y = df["theta1"][sparser][::4]
 
     # Plot the line first
-    ax.plot(df["x"], df["model"], color="red", linewidth=1.75, label="Modelo \n q     = {:.2f}\n $\Delta$ = {:.2f}".format(popt[0],popt[1]),zorder=2)
+    ax.plot(df["x"], df["model"], color="red", linewidth=1.75, label="Modelo \n q     = {:.2f}\n $\Delta$ = {:.2f}".format(q,popt[1]),zorder=2)
 
     #ax.plot(df["x"][::8],df["theta1"][::8],'o', markersize=3.5, markerfacecolor='none', label='Simulación',zorder=10,color="black")
     ax.plot(sparsel_x,sparsel_y,'o', markersize=3.5, markerfacecolor='none',zorder=10,color="black")
@@ -94,14 +99,14 @@ def ModelFit(filepath,i):
     
     # Add grid, labels, and ticks
     
-    xticks = np.arange(0, 513, 32)
-    xlabels = xticks - 256  # Shift center to 0
+    xticks = np.arange(0, rows+1, int(rows/8))
+    xlabels = xticks - rows/2  # Shift center to 0
 
     ax.set_title('{}'.format(title))
     ax.set_xticks(xticks)
     ax.set_xticklabels(xlabels)
 
-    plt.xlim([128,384])
+    plt.xlim([rows/4,3*rows/4])
     
     if isdiff:
         ax.set_yticks(np.arange(15,16,60))
@@ -123,7 +128,7 @@ def ModelFit(filepath,i):
     #ax.set_ylabel(r'$\theta$', rotation=0)
     
     # Add a vertical line at x=256
-    ax.axvline(x=256, color='black', linestyle='--', linewidth=0.8)
+    ax.axvline(x=rows/2, color='black', linestyle='--', linewidth=0.8)
     
     l =ax.legend(loc='lower right',bbox_to_anchor=(0.99, 0.2))
     l.set_zorder(15)
@@ -139,8 +144,10 @@ def ModelFit(filepath,i):
     # Save params
     f = open(os.path.join(directory, "params_{}.txt".format(i)),"w")
     f.write("{}\n".format(title))
-    f.write("q = {}\n".format(popt[0]))
+    f.write("q = {}\n".format(q))
     f.write("Delta = {}\n".format(popt[1]))
+    f.write("q_err = {}\n".format(Dq))
+    f.write("Delta_err = {}\n".format(Dd))
     f.write("TH1 = {}\n".format(df["theta1"][0]))
     f.write("TH2 = {}".format(df["theta1"].values[-1]))
     f.close
@@ -158,7 +165,7 @@ if __name__ == "__main__":
     for root, dirs, files in os.walk(rootdir,topdown=True):
         for i in range(3):
             tabdir = os.path.join(root,'segment_{0}.txt'.format(i))
-            imgdir = os.path.join(root,'model_{0}.png'.format(i))
+            imgdir = os.path.join(root,'segment_{0}_model.png'.format(i))
             if os.path.isfile(imgdir) and not args.force:
                 print(f"The file '{imgdir}' is already rendered. Skipping...")
                 pass

paramtable_split.py
@@ -6,12 +6,16 @@ def ParamFetch(filepath,i):
     with open(filepath, 'r') as f:
         lines = f.readlines()
 
-    q = delta = a11 = a12 = kb = th1 = th2 = None
+    q = delta = Dq = Ddelta = a11 = a12 = kb = th1 = th2 = None
     for i, line in enumerate(lines):
         if 'q =' in line:
             q = np.float32(line.split()[-1])
         elif 'Delta =' in line:
             delta = np.float32(line.split()[-1])
+        elif 'q_err =' in line:
+            Dq = np.float32(line.split()[-1])
+        elif 'Delta_err =' in line:
+            Ddelta = np.float32(line.split()[-1])
         elif 'TH1 =' in line:
             th1 = np.float32(line.split()[-1])
         elif 'TH2 =' in line:
@@ -34,18 +38,18 @@ def ParamFetch(filepath,i):
             kb = np.float32(line.split()[-1])
         pass
     f.close()
-    return q, delta, a11, a12, kb, th1, th2
+    return q, delta, Dq, Ddelta, a11, a12, kb, th1, th2
 
 rootdir = os.path.abspath(os.path.dirname( __file__ ))
 #rootdir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 mainfile = os.path.join(rootdir,'paramtable.txt')
 
 table = open(mainfile, 'w')
-table.write("q d A11 A12 K3B TH1 TH2 seg\n")
+table.write("q d Dq Dd A11 A12 K3B TH1 TH2 seg\n")
 
 for i in range(3):
     for root, dirs, files in os.walk(rootdir,topdown=True):
         tabdir = os.path.join(root,'params_{}.txt'.format(i))
         if os.path.isfile(tabdir):
             Params = ParamFetch(tabdir,i)
-            table.write("{0} {1} {2} {3} {4} {5} {6} {7}\n".format(Params[0],Params[1],Params[2],Params[3],Params[4],Params[5],Params[6],i))
+            table.write("{0} {1} {2} {3} {4} {5} {6} {7} {8} {9}\n".format(Params[0],Params[1],Params[2],Params[3],Params[4],Params[5],Params[6],Params[7],Params[8],i))